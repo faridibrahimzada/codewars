e8805dc6774432b3b23550f1c7787d28e56b9d61
faridibrahimzada <ibrahimzada.farid@gmail.com>
2026-04-23 20:55:36 +0400

    add codewars solution (8kyu, How good are you really?)

---
+++ 8kyu/how_good_are_you_really.py
@@ -0,0 +1,14 @@
+"""
+Language: Python
+Kata: How good are you really?
+Kyu: 8
+Description: There was a test in your class and you passed it. Congratulations! But you're an ambitious person. You want to know if you're better than the average student in your class. You receive an array with your peers' test scores. Now calculate the average and compare your score! Return true if you're better, else false!
+URL: https://www.codewars.com/kata/5601409514fc93442500010b
+
+"""
+
+# Solution
+
+
+def better_than_average(class_points, your_points):
+    return your_points > sum(class_points) / len(class_points)
