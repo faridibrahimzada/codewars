31ba185730010236e16bf8d2245fa9f39babb241
faridibrahimzada <ibrahimzada.farid@gmail.com>
2026-04-26 18:02:35 +0400

    add codewars solution (8kyu, Convert a Boolean to a String)

---
+++ 8kyu/convert_boolean_to_string.py
@@ -0,0 +1,15 @@
+"""
+Language: Python
+Kata: Convert a Boolean to a String
+Kyu: 8
+Description: Implement a function which convert the given boolean value into its string representation.
+URL: https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+"""
+
+
+# Solution
+
+
+def boolean_to_string(b):
+    return str(b)
